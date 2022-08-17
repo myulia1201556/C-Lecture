0d1284af50efdf72f106faf270413a8eda3a98a5
Yulia <yulia1201556@gmail.com>
2022-08-17 10:00:51 +0300

    вывод массива из  N элементов

--- task 30/Program.cs
+++ task 30/Program.cs
@@ -3,3 +3,28 @@
 // нулями и единицами в случайном порядке.
 // [1,0,1,1,0,1,0,0]
 
+Console.WriteLine("Введите размер массива: ");
+int lenght = Convert.ToInt32(Console.ReadLine());
+
+int[] RandomArray(int len)
+{
+int[] arrays = new int[len];
+for (int i = 0; i < arrays.Length; i++)
+{
+    arrays[i] = new Random().Next(0, 2);
+}
+return arrays;
+}
+int[] result = RandomArray(lenght);
+
+void PrintArray(int[] result1)
+{
+    Console.Write($"[ ");
+    for (int i = 0; i < result1.Length; i++)
+    {
+     Console.Write($"{result1[i]} ");   
+    }
+    Console.Write($"]");
+}
+PrintArray(result);
+
