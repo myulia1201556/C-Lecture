028e9ba57e01cf03c1b26adebaba7df35d7a9be2
Yulia <yulia1201556@gmail.com>
2022-08-23 16:35:11 +0300

    Преобразование десятичного числа в двоичное

--- task42/Program.cs
+++ task42/Program.cs
@@ -4,3 +4,26 @@
 // 3 -> 11
 // 2 -> 10
 
+Console.Clear();
+System.Console.Write("Введите десятичное чиcло -> ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int TransToBinary(int num)
+{
+    int result = 0;
+    int i = 1;
+
+    while (num > 0)
+    {
+        result += num % 2 * i;
+        num /= 2;
+        i *= 10;
+    }
+
+    return result;
+
+}
+
+int transToBinary = TransToBinary(number);
+
+Console.WriteLine($"Бинарное -> {transToBinary}");
