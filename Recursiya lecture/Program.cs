dc50f0651df57bea6fda2671f919198ec2ede10e
Yulia <yulia1201556@gmail.com>
2022-08-31 13:54:04 +0300

    Сумма индексов матрицы

---
+++ Recursiya lecture/Program.cs
@@ -0,0 +1,102 @@
+﻿string[,] table = new string[2, 5];
+
+// String.Empty;
+// table[0,0]     table[0,1]    table[0,2]    table[0,4]
+// table[1,0]     table[1,1]    table[1,2]    table[1,4]
+
+// table[1,2] = "слово";
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int colum = 0; colum < 5; colum++)
+//     {
+//         Console.WriteLine($"-{table[rows, colums]}-");
+//     }
+// }
+
+// int [,] pic = new int[, ];
+
+// for (int i = 0; i < 3; i++)
+// {
+//  for (int j = 0; j < 4; j++)
+//  {
+//      Console.Write($"{matrix[i, j]} ");
+//  }   
+//  Console.WriteLine();
+// }
+
+
+
+
+
+// void PrintImage(int[,] image)
+// {
+//     for (int i = 0; i < image.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < image.GetLength(1); j++)
+//         {
+//             // Console.Write($"{image[i, j]} ");
+//             if(image[i, j] == 0) Console.Write($" ");
+//             else Console.Write($"+");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// PrintImage(pic);
+
+// void FillArray(int[,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             matr[i, j] = new Random().Next(1, 10); // [1, 10)
+//         }
+//     }
+// }
+// int[,] matrix = new int[3, 4];
+// PrintArray(matrix);
+// FillArray(matrix);
+// Console.WriteLine();
+// PrintArray(matrix);
+
+
+
+
+
+// метод который принимает число, факториал которого нужно вычислить
+
+// double Factorial(int n)
+// {
+//     // 1! = 1
+//     // 0! = 1
+//     if (n == 1) return 1;
+//     else return n * Factorial(n - 1);
+// }
+// for (int i = 1; i < 40; i++)
+// {
+//     Console.WriteLine($"{i}! = {Factorial(i)}");     //1*2*3 = 6
+//}
+
+
+
+
+//Метод вычисления чисел Фибоначчи
+
+// f(1) = 1
+// f(2) = 2
+
+// f(n) = f(n - 1) + f(n - 2)
+
+
+double Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"{i} = {Fibonacci(i)}");
+}
---
+++ task48/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Amn = m+n.
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+Console.Clear();
+
+Console.WriteLine("Введите число строк m:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов n:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m == 0 || n == 0)
+{
+    Console.WriteLine("Невернозаданны параметры матрицы, попробуйте снова");
+    return;
+}
+
+int[,] CreateMatrixSumIndex(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+
+            matrix[i, j] = i +j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j]}, ");
+            else Console.Write($"{matrix[i, j]}");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+Console.WriteLine($"Двумерный массив m x n: ");
+int[,] array2d = CreateMatrixSumIndex(m, n);
+PrintMatrix(array2d);
